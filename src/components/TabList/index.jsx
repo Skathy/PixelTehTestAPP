c58e5b53e5e8d98011cdb52b6cc954fc7763edbb
Oleg <chibikstrong@gmail.com>
2021-07-23 10:16:35 +0300

    tablist hardcode removed

--- src/components/TabList/index.jsx
+++ src/components/TabList/index.jsx
@@ -6,49 +6,33 @@ import { BeerList } from '../BeerList';
 
 export const TabList = ({clkHandler, sorting, sortBy, currentTab, page, perPage, nextPage, prevPage}) => {
     const { TabPane } = Tabs;
-    const { pizza, steak, beers, isLoading } = useSelector(state => state.beerReducer)
+    const { beerReducer } = useSelector(state => state)
+    
+    const tabs = {[0]: 'PIZZA', [1]: 'STEAK', [2]: 'ALL'}
 
     return (
         <Tabs 
                 defaultActiveKey="1" 
                 onTabClick={key => clkHandler( key)}
-            >
-                <TabPane tab="Pizza" key="1" disabled={isLoading}>
-                    <BeerList 
-                        sorting={sorting}
-                        sortParam={sortBy}
-                        loading={isLoading}
-                        beers={pizza}
-                        perPage={perPage[currentTab]}
-                        currentPage={page[currentTab]}
-                        incrementHandler={nextPage}
-                        decrementHandler={prevPage}
-                        />
-                </TabPane>
-                <TabPane tab="Steak" key="2" disabled={isLoading}>
-                    <BeerList
-                        sorting={sorting}
-                        sortParam={sortBy}
-                        loading={isLoading}
-                        beers={steak}
-                        perPage={perPage[currentTab]}
-                        currentPage={page[currentTab]}
-                        incrementHandler={nextPage}
-                        decrementHandler={prevPage}
-                        />
-                </TabPane>
-                <TabPane tab="ALL" key="3" disabled={isLoading}>
-                    <BeerList
-                        sorting={sorting}
-                        sortParam={sortBy}
-                        loading={isLoading} 
-                        beers={beers}
-                        perPage={perPage[currentTab]}
-                        currentPage={page[currentTab]}
-                        incrementHandler={nextPage}
-                        decrementHandler={prevPage}
-                        />
-                </TabPane>
+            >   
+                {Object.keys(beerReducer).map((item, index) => {
+                    if (Array.isArray(beerReducer[item])) {
+                        return (
+                            <TabPane tab={tabs[index]} key={index+1 }>
+                                    <BeerList
+                                        sorting={sorting}
+                                        sortParam={sortBy}
+                                        loading={beerReducer.isLoading}
+                                        beers={beerReducer[item]}
+                                        perPage={perPage[currentTab]}
+                                        currentPage={page[currentTab]}
+                                        incrementHandler={nextPage}
+                                        decrementHandler={prevPage}
+                                    />
+                            </TabPane>
+                        )
+                    }
+                })}
             </Tabs>
     )
 }
